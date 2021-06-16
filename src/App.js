6b07d6ad22bd806442a0ed8c5cc3028d2dbe46ed
Ajinkyap22 <palaskarap15@gmail.com>
2021-06-16 20:23:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import first from "./images/level-1.jpeg";
+// import second from "./images/level-2.jpg";
+// import third from "./images/level-3.jpg";
+// import fourth from "./images/level-4.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img src="" alt="" className="img-fluid" />
+      </div>
     </div>
   );
 }
