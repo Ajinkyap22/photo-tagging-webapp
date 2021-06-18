943669f6c2643c855d1005c20a285fe1ee32376b
Ajinkyap22 <palaskarap15@gmail.com>
2021-06-18 16:22:27 +0530

    Started timer implementation

--- src/Components/modal.js
+++ src/Components/modal.js
@@ -1,14 +1,31 @@
 function Modal(props) {
   return (
-    <div className="charModal" hidden={props.show ? false : true}>
-      <button className="close-modal" onClick={props.closeModal}>
-        X
-      </button>
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">Char 1</li>
-        <li className="list-group-item">Char 1</li>
-        <li className="list-group-item">Char 1</li>
-      </ul>
+    <div className={props.show ? "modal show" : "modal"}>
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Characters</h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">Character 1</li>
+              <li className="list-group-item">Character 2</li>
+              <li className="list-group-item">Character 3</li>
+            </ul>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-primary">
+              OK
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
